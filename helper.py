424433025e6407a583a3a305b01c677d5c105aa7
LaRena Iocco <rosie.mcqueen@gmail.com>
2021-02-25 12:28:32 -0800

    testing commits on datadog

--- helper.py
+++ helper.py
@@ -1,16 +1,21 @@
-from ddtrace import patch_all; patch_all(logging=True)
-import logging
-from ddtrace import tracer
+# from ddtrace import patch_all; patch_all(logging=True)
+# import logging
+# from ddtrace import tracer
 
-FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
-          '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
-          '- %(message)s')
-logging.basicConfig(format=FORMAT)
-log = logging.getLogger(__name__)
-log.level = logging.INFO
+# FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+#           '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+#           '- %(message)s')
+# logging.basicConfig(format=FORMAT)
+# log = logging.getLogger(__name__)
+# log.level = logging.INFO
 
-@tracer.wrap()
-def hello():
-    log.info('Hello, World!')
+# @tracer.wrap()
+# def hello():
+#     log.info('Hello, World!')
 
-hello()
+# hello()
+
+def sum(a, b)
+    return a + b)
+
+sum(1, 2)
